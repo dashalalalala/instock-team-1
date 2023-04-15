5e1b681285f537e450802f13e115f2f9626583be
adrienbelcastro <adrienbelcastro1@gmail.com>
2023-04-15 15:40:53 -0400

    created footer component and made it responsive

---
+++ src/components/Footer/footer.js
@@ -0,0 +1,12 @@
+import "./footer.scss";
+const Footer = () => {
+  return (
+    <>
+      <div className="footer">
+        <p className="footer__content">© InStock Inc. All Rights Reserved</p>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
