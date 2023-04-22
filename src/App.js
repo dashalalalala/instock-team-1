12a180bf47db19339648ce11ee2e866e01be8a8e
adrienbelcastro <adrienbelcastro1@gmail.com>
2023-04-21 21:51:36 -0400

    create/style edit warehouse page & form component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import "./App.css";
 import Footer from "./components/Footer/footer";
 import Header from "./components/Header/header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import EditWarehouse from "./Pages/EditWarehouse/EditWarehouse";
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <Header />
+      <Routes>
+        <Route path="/" element={<EditWarehouse />} />
+      </Routes>
       <Footer />
-    </div>
+    </BrowserRouter>
   );
 }
 
