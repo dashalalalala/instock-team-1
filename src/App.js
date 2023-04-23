f6eb33491b789ebe50dd73162e3ff4b23ddd2bcb
adrienbelcastro <adrienbelcastro1@gmail.com>
2023-04-22 20:19:28 -0400

    fixed put request not working & renamed components

--- src/App.js
+++ src/App.js
@@ -4,27 +4,35 @@ import Header from "./Components/Header/header";
 import WarehouseDetailsPage from "./Pages/WarehouseDetailsPage/WarehouseDetailsPage";
 import WarehouseListPage from "./Pages/WarehouseListPage/WarehouseListPage";
 import InventoryListPage from "./Pages/InventoryListPage/InventoryListPage";
+import EditWarehousePage from "./Pages/EditWarehouse/EditWarehouse";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "./styles/theme.js";
 
 function App() {
-	return (
-		<ChakraProvider theme={theme}>
-			<div className="App">
-				<Header />
-				<BrowserRouter>
-					<Routes>
-						<Route path="/" element={<WarehouseListPage />} />
-						<Route path="/warehouses" element={<WarehouseListPage />} />
-						<Route path="/warehouses/:warehouseId" element={<WarehouseDetailsPage />} />
-						<Route path="/inventories" element={<InventoryListPage />} />
-					</Routes>
-				</BrowserRouter>
-				<Footer />
-			</div>
-		</ChakraProvider>
-	);
+  return (
+    <ChakraProvider theme={theme}>
+      <div className="App">
+        <Header />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<WarehouseListPage />} />
+            <Route path="/warehouses" element={<WarehouseListPage />} />
+            <Route
+              path="/warehouses/:warehouseId"
+              element={<WarehouseDetailsPage />}
+            />
+            <Route
+              path="/warehouses/:warehouseId/edit"
+              element={<EditWarehousePage />}
+            />
+            <Route path="/inventories" element={<InventoryListPage />} />
+          </Routes>
+        </BrowserRouter>
+        <Footer />
+      </div>
+    </ChakraProvider>
+  );
 }
 
 export default App;
